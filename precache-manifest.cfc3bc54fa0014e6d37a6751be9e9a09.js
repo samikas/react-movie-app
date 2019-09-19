f765de682ad10f8753b2beae4a1db10746a600e3
samikas <samppa11@hotmail.com>
2019-09-19 20:52:30 +0300

    Updates

---
+++ precache-manifest.cfc3bc54fa0014e6d37a6751be9e9a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85b5ae6cb1ce7df208b297cc97caf49",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "c7cff4393fe44fd1f508",
+    "url": "/react-movie-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9592d62c5a40c6b172ef",
+    "url": "/react-movie-app/static/css/main.0927082e.chunk.css"
+  },
+  {
+    "revision": "c7cff4393fe44fd1f508",
+    "url": "/react-movie-app/static/js/2.cfd932b2.chunk.js"
+  },
+  {
+    "revision": "9592d62c5a40c6b172ef",
+    "url": "/react-movie-app/static/js/main.cfafc1e0.chunk.js"
+  },
+  {
+    "revision": "431cf5a7ad68fc8a50db",
+    "url": "/react-movie-app/static/js/runtime~main.69303c14.js"
+  }
+]);
